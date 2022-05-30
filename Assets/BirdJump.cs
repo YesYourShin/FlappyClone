54bf5623c6a0d1704419921df1f4c50e60711562
YesYourShin <ehrl827@naver.com>
2022-05-30 11:51:51 +0900

    add Bird Jump

---
+++ Assets/BirdJump.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BirdJump : MonoBehaviour
+{
+
+    Rigidbody2D rb;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetMouseButton(0))
+        {
+            rb.velocity = Vector2.up * 3; // (0, 1) * 3
+        }
+    }
+}
